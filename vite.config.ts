fc0da20dc7e8640525967b86e6c56b3e0309babc
Stanislav Savinetskii <stanislav.savinetskii@globemee.com>
2025-01-19 18:27:10 +0500

    gh pages2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Voron210/', // Замените на имя вашего репозитория
+  base: '/react3d/',
   plugins: [react()],
 })
